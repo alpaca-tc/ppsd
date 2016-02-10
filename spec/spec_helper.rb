41d41fd2aaa9922f224508b2328c8d300f6bac2a
alpaca-tc <alpaca-tc@alpaca.tc>
2016-02-10 19:31:33 +0900

    Add spec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,29 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'ppsd'
 require 'pry'
+
+RSpec.configure do |config|
+  config.raise_errors_for_deprecations!
+  config.order = :random
+  config.profile_examples = 10
+
+  unless ENV['CIRCLECI']
+    config.filter_run :focus
+    config.run_all_when_everything_filtered = true
+  end
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
